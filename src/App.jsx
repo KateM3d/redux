baf6e5d3b62e02d17e9e0de98bd9f3bdbbb661d2
KateM3d <ekaterina8medvedeva@gmail.com>
2022-03-12 13:40:01 -0500

    redux basic counter

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useSelector, useDispatch } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  decrementByAmount,
+  multiplyByAmount,
+} from "./redux/counter";
+
+function App() {
+  const { value } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <p>Score: {value}</p>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+      <button onClick={() => dispatch(decrementByAmount(5))}>-5</button>
+      <button onClick={() => dispatch(multiplyByAmount(5))}>x5</button>
+    </div>
+  );
+}
+
+export default App;
